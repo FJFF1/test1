ef115325b84082df859a4717f6bd37c43aae0bed
FJFF1 <fj7648ob@gmail.com>
2025-03-27 16:54:23 +0300

    PR 3 Animals

---
+++ src/feature3/Animal.java
@@ -0,0 +1,30 @@
+package feature3;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Dog A1 = new Dog();
+//        A1.name = "A1";
+//        A1.age = 5;
+//        A1.bark();
+//
+//        Cat B1 = new Cat();
+//        B1.name = "B1";
+//        B1.age = 2;
+//        B1.meow();
+//
+//    }
+//}
+
+public class Animal {
+
+    public String name;
+    public double age;
+
+    public void makeSound() {
+        System.out.println( " издает звук ");
+    }
+
+
+}
+
