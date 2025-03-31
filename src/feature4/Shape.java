8f64496763c2595040230f1283bc8456cf1c4a5d
FJFF1 <fj7648ob@gmail.com>
2025-03-31 13:24:54 +0300

    PR 4 FIGURE

---
+++ src/feature4/Shape.java
@@ -0,0 +1,17 @@
+package feature4;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Shape rectangle = new Rectangle(10.5, 10);
+//        Shape circle = new Circle(10);
+//
+//
+//        System.out.println("Площадь прямоугольника = " + rectangle.calculateArea());
+//        System.out.println("Площадь круга = " + circle.calculateArea());
+//    }
+//}
+
+public abstract class Shape {
+
+    public abstract double calculateArea ();}
