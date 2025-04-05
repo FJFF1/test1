3db611cadfc48424dba08b6b92f83c07cdd56a35
FJFF1 <fj7648ob@gmail.com>
2025-04-05 15:02:34 +0300

    PR7 library

---
+++ src/feature7/Main.java
@@ -0,0 +1,33 @@
+package feature7;
+
+public class Main {
+    public static void main(String[] args){
+
+        Library library = new Library();
+
+        Book book1 = new Book("В поисках утраченного времени", "Марсель Пруст");
+        Book book2 = new Book("Сто лет одиночества","Габриэль Гарсиа Маркес");
+        Book book3 = new Book("Лолита", "Владимир Набоков");
+        Book book4 = new Book("1984","Джордж Оруэлл");
+        Book book5 = new Book("Анна Каренина", "Лев Толстой");
+
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+
+
+
+        for (Book book : library.displayBooks()){
+            System.out.println("Название: " + book.getTitle() +". Автор: " + book.getAuthor());
+    }
+
+        library.removeBook(book4);
+        System.out.println("Книга "+ book4.getTitle() + " - удалена.");
+        for (Book book : library.displayBooks()) {
+            System.out.println("Название: " + book.getTitle() + ". Автор: " + book.getAuthor());
+        }
+}
+}
