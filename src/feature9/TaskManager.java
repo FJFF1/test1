c98b565305481689b12d110abd6ee946885ef7a7
FJFF1 <fj7648ob@gmail.com>
2025-04-12 12:17:32 +0300

    PR9 Taskmanager

---
+++ src/feature9/TaskManager.java
@@ -0,0 +1,27 @@
+package feature9;
+
+import
+
+public class TaskManager {
+    Map<Int, Task> tasks;
+
+    public void addTask(Int number, Task task) {
+
+    }
+
+    void runTaskByNumber(Int task) {
+
+    }
+
+
+
+
+}
+
+
+//        addTask(Int number, Task task) - добавляет новую таску в мапу. Здесь можно использовать анонимные классы
+//        runTaskByNumber(Int number) - реализовать метод запуска задания
+//        добавить статичное поле taskManager в класс Main
+//        сделать метод static void initTasks() в классе Main - написать логику добавления заданий в таск менеджер, где номер задания это ключ, а реализованный класс Task это значение. Для тестирования каждой задачи описать логики методе run()
+//
+//        в методе main написать логику тестирования заданий пользователем, используя консоль для ввода
